d8c9eada1e620ac8740a5090b2d775e8b3374bc9
stack <stack@gitlab.helios.local>
2023-07-08 13:42:33 -0400

    Add elf parsing and GOT-grabbing functionality to code. Split out to libs, eventually

--- src/got/gotfreed.c
+++ src/got/gotfreed.c
@@ -1,4 +1,117 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <elf.h>
+
+#define COUNT 20
+#define ELFLEN 18000 // TODO: Make this a more sane value after testing
+
+Elf64_Addr get_got(unsigned char* elf, unsigned long* addr) {
+  // Get header
+  Elf64_Ehdr* elf_header = (Elf64_Ehdr*)(elf);
+
+  // Get program headers
+  Elf64_Phdr* p_headers = (Elf64_Phdr*)(elf + elf_header->e_phoff);
+
+  // Find dynamic segment
+  Elf64_Phdr* dyn_seg = NULL;
+  for (int i = 0; i < elf_header->e_phnum; i++) {
+    if (p_headers[i].p_type == PT_DYNAMIC) {
+      dyn_seg = &p_headers[i];
+      break;
+    }
+  }
+  if (!dyn_seg) {
+    printf("Could not find dynamic segment...\n");
+    exit(1);
+  }
+
+  // Get dynamic section from segment
+  Elf64_Dyn* dyn_sec = (Elf64_Dyn*)(elf + dyn_seg->p_offset);
+
+  // Search through dynamic section for GOT
+  Elf64_Addr got = 0;
+  for (int i = 0; dyn_sec[i].d_tag != DT_NULL; i++) {
+    if (dyn_sec[i].d_tag == DT_PLTGOT) {
+      got = dyn_sec[i].d_un.d_ptr;
+      break;
+    }
+  }
+  if (!got) {
+    printf("Could not find the GOT...\n");
+    exit(1);
+  }
+
+  // If the GOT was find-able, we probably just got the offset... 
+  // Calculate the *rel* GOT
+  got = got + *addr + 24; // 24 is the size of the GOT header we dont' care about
+
+  return got;
+}
+
+int validate_elf(unsigned char* buf) {
+  if ((buf[0] != '\x7f') || (buf[1] != 'E') || (buf[2] != 'L') || (buf[3] != 'F')) {
+    printf("Either this isn't an ELF or you are reading the wrong part of memory...\n");
+    return 1;
+  }
+  return 0;
+}
+
+void parse_elf(int* pid, unsigned long* addr) {
+  int memsize = 50;
+  char* mem = malloc(memsize);
+  sprintf(mem, "/proc/%d/mem", *pid);
+
+  int fd_mem = open(mem, O_RDWR);
+  if (fd_mem == -1) {
+    printf("Failed to open mem file\n");
+    exit(1);
+  }
+
+  unsigned char* elf = malloc(ELFLEN);
+  if (elf == NULL) {
+    printf("Couldn't allocate %d bytes...\n", ELFLEN);
+    exit(1);
+  }
+
+  lseek(fd_mem, *addr, SEEK_SET);
+  read(fd_mem, elf, ELFLEN);
+
+  int fail = validate_elf(elf);
+  if (fail) {
+    printf("Failed to validate ELF...\n");
+    exit(1);
+  }
+
+  Elf64_Addr* got = malloc(sizeof(Elf64_Addr));
+  *got = get_got(elf, addr);
+
+  printf("First GOT entry is: %p\n", (void *)*got);
+}
 
 int main(int argc, char** argv) {
+  if (argc != 3) {
+    printf("Usage: %s <addr to read from>\n", argv[0]);
+    return 1;
+  }
+
+  int* pid = malloc(sizeof(int));
+  *pid = strtol(argv[1], NULL, 10);
+  if (errno == ERANGE || errno == EINVAL || *pid == 0) {
+    printf("Something went wrong with the PID you entered...\n");
+    return 1;
+  }
+
+  unsigned long* addr = malloc(sizeof(unsigned long));
+  *addr = strtoul(argv[2], NULL, 16);
+  if (errno == ERANGE || errno == EINVAL || *addr == 0) {
+    printf("Something went wrong with the address you entered...\n");
+    return 1;
+  }
+
+  parse_elf(pid, addr);
+
+  return 0;
 }
